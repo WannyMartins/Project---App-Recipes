214c29dc49c412767a29659f108f5b07fa07e0a8
isabelanery <isabelanery242@gmail.com>
2022-04-27 15:43:05 -0300

    arruma erros dos Links no Header

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,17 +1,16 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 function SearchBar() {
   return (
     <>
-      <Link to="/search" className="search-bar" href="link to searchBar">
+      <button className="search-bar" type="button">
         <img
           src="../images/searchIcon.svg"
           alt="search"
           data-testid="search-top-btn"
           width="30px"
         />
-      </Link>
+      </button>
 
       <div />
     </>
