62e379ab4424d05ebfca4740a4b52823d2e153ee
IanSilveira <iansilveira2014@hotmail.com>
2022-04-27 11:37:25 -0300

    Corrigido!

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -2,29 +2,26 @@ import React from 'react';
 import { useHistory } from 'react-router';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
-import Buttons from '../components/Buttons';
+import Button from '../components/Buttons';
 
 function Explore() {
   const history = useHistory();
   return (
     <>
       <Header />
-      <div>
-        <button
-          data-testid="explore-foods"
-          type="button"
+      <div className="exploreButtons">
+        <Button
+          text="Explore Foods"
+          className="exploreFoodsButton"
+          dataTestID="explore-foods"
           onClick={ () => history.push('/explore/foods') }
-        >
-          Explore Foods
-        </button>
-        <button
-          data-testid="explore-drinks"
-          type="button"
+        />
+        <Button
+          text="Explore Drinks"
+          className="exploreDrinksButton"
+          dataTestID="explore-drinks"
           onClick={ () => history.push('/explore/drinks') }
-        >
-          Explore Drinks
-        </button>
-        <Buttons />
+        />
       </div>
       <Footer />
     </>
