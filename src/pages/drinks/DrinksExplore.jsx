abaaca023b898834d62e53f92e3c91dd575329a0
Bruno Minelli <minellibruno@hotmail.com>
2022-04-28 16:39:12 -0300

    Requisitos 70, 71, 72 e 73 concluídos.

--- src/pages/drinks/DrinksExplore.jsx
+++ src/pages/drinks/DrinksExplore.jsx
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
 import Button from '../../components/Buttons';
 
 function DrinksExplore() {
+  const [randomDrink, setRandomDrink] = useState([]);
+  const history = useHistory();
+  const endpoint = 'https://www.thecocktaildb.com/api/json/v1/1/random.php';
+
+  useEffect(() => {
+    const fetchRandomDrink = async () => {
+      const response = await fetch(endpoint);
+      const data = await response.json();
+      setRandomDrink(data.drinks);
+    };
+    fetchRandomDrink();
+  }, []);
+
   return (
     <>
       <Header tittle="Explore Drinks" />
@@ -11,10 +25,12 @@ function DrinksExplore() {
         <Button
           text="By Ingredient"
           dataTestId="explore-by-ingredient"
+          onClick={ () => history.push('/explore/drinks/ingredients') }
         />
         <Button
           text="Surprise me!"
           dataTestId="explore-surprise"
+          onClick={ () => console.log(randomDrink) }
         />
       </main>
       <Footer />
--- src/pages/foods/FoodsExplore.jsx
+++ src/pages/foods/FoodsExplore.jsx
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
 import Button from '../../components/Buttons';
 
 function FoodsExplore() {
+  const [randomFood, setRandomFood] = useState([]);
+  const history = useHistory();
+  const endpoint = 'https://www.themealdb.com/api/json/v1/1/random.php';
+
+  useEffect(() => {
+    const fetchRandomFood = async () => {
+      const response = await fetch(endpoint);
+      const data = await response.json();
+      setRandomFood(data.meals);
+    };
+    fetchRandomFood();
+  }, []);
+
   return (
     <>
       <Header tittle="Explore Foods" />
@@ -11,14 +25,17 @@ function FoodsExplore() {
         <Button
           text="By Ingredient"
           dataTestId="explore-by-ingredient"
+          onClick={ () => history.push('/explore/foods/ingredients') }
         />
         <Button
           text="By Nationality"
           dataTestId="explore-by-nationality"
+          onClick={ () => history.push('/explore/foods/nationalities') }
         />
         <Button
           text="Surprise me!"
           dataTestId="explore-surprise"
+          onClick={ () => console.log(randomFood) }
         />
       </main>
       <Footer />
