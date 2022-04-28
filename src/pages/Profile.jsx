9791114485c326855e0aef9b0d1deef2fa0a1449
Bruno Minelli <minellibruno@hotmail.com>
2022-04-28 10:12:59 -0300

    Preparação de ambiente para desenvolvimento do grupo de requisitos Tela de Perfil.

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 function Profile() {
   return (
-    <div>
+    <>
       <Header tittle="Profile" />
-      Profile
-    </div>
+      <main>
+        Profile
+      </main>
+      <Footer />
+    </>
   );
 }
 
