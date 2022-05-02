ec77ce7a17e0ce7e6f18c670eeed6a939274bc0d
Bruno Minelli <minellibruno@hotmail.com>
2022-05-02 16:26:28 -0300

    Preparação de ambiente para o grupo de requisitos Tela de explorar nacionalidades e requisito 78 concluído.

--- src/pages/foods/FoodExploreNationalities.jsx
+++ src/pages/foods/FoodExploreNationalities.jsx
@@ -1,17 +1,44 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from '../../components/Header';
 import SearchBar from '../../components/SearchBar';
 import Footer from '../../components/Footer';
+import { fetchMealsExplore } from '../../services/apis';
+import FoodsList from '../../components/FoodsList';
 
 function FoodExploreNationalities() {
+  const [nacionalities, setNacionalities] = useState([]);
+
+  useEffect(() => {
+    const fetchNacionalities = async () => {
+      const data = await fetchMealsExplore('areas');
+      setNacionalities(data);
+    };
+    fetchNacionalities();
+  }, []);
+
   return (
-    <div>
+    <>
       <Header tittle="Explore Nationalities">
         <SearchBar />
       </Header>
-      FoodExploreNationalities
+      <main>
+        <section>
+          <select data-testid="explore-by-nationality-dropdown">
+            {nacionalities.map((nacionality, index) => (
+              <option
+                key={ `${index}-nacionality` }
+                value={ nacionality.strArea }
+                data-testid={ `${nacionality.strArea}-option` }
+              >
+                { nacionality.strArea }
+              </option>
+            ))}
+          </select>
+        </section>
+        <FoodsList />
+      </main>
       <Footer />
-    </div>
+    </>
   );
 }
 
