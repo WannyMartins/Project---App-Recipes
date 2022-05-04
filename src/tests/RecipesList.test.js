03e16a210f73c589d31d917c1f022d86e80b5e4a
Wanny Martins <wcatery@gmail.com>
2022-05-04 11:10:35 -0300

    Confirmar merge em g16-testes

---
+++ src/tests/RecipesList.test.js
@@ -0,0 +1,21 @@
+import { screen } from '@testing-library/react';
+import React from 'react';
+// import FoodsList from '../components/FoodsList';
+// import FoodsList from '../components/FoodsList';
+import RecipesProvider from '../context/recipesProvider';
+import renderWithRouter from '../helpers/renderWithRouter';
+import Foods from '../pages/foods/Foods';
+
+// const TWELVE = 12;
+
+describe('Foods', () => {
+  it('Verifica a tela Foods', () => {
+    renderWithRouter(
+      <RecipesProvider>
+        <Foods />
+      </RecipesProvider>,
+    );
+
+    screen.queryAllByText(/foods/i);
+  });
+});
