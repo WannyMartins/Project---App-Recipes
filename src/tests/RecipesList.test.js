5c24bdc7f86e86bdf146cdb67b38ba9947649543
Wanny Martins <wcatery@gmail.com>
2022-05-09 16:07:05 -0300

    testes em andamento com header e searchBar ok

--- src/tests/RecipesList.test.js
+++ src/tests/RecipesList.test.js
@@ -1,4 +1,4 @@
-import { fireEvent, screen } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import React from 'react';
 // import FoodsList from '../components/FoodsList';
@@ -6,15 +6,14 @@ import RecipesProvider from '../context/recipesProvider';
 import renderWithRouter from '../helpers/renderWithRouter';
 import Foods from '../pages/foods/Foods';
 
-function mock() {
-  global.alert = jest.fn()
-    .mockReturnValue('Sorry, we haven/\'t found any recipes for these filters.');
-}
 const TWELVE = 12;
-const alert = 'Sorry, we haven/\'t found any recipes for these filters.';
+async function alert() {
+  return screen
+    .findAllByText('Sorry, we haven/\'t found any recipes for these filters.');
+}
 
 describe('Foods', () => {
-  it('Verifica a tela Foods há 12 cards', async () => {
+  it.skip('Verifica a tela Foods há 12 cards', async () => {
     renderWithRouter(
       <RecipesProvider>
         <Foods />
@@ -25,7 +24,31 @@ describe('Foods', () => {
     expect(cards.length).toEqual(TWELVE);
   });
 
-  it('Verifica a tela Foods há botoes de categorias', async () => {
+  it('Verifica se ao clicar na receita encaminha para details', async () => {
+    const { history } = renderWithRouter(
+      <RecipesProvider>
+        <Foods />
+      </RecipesProvider>,
+    );
+    const firstRecipe = await screen.findByTestId('0-card-img');
+    expect(firstRecipe).toBeInTheDocument();
+    userEvent.click(firstRecipe);
+
+    const { pathname } = history.location;
+
+    expect(pathname).toBe('/foods/52977');
+
+    // console.log(firstRecipe);
+    // const name = screen.getByLabelText('Name');
+    // const inputSearch = screen.getByTestId('search-input');
+    // fireEvent.change(inputSearch, { target: { value: '/////' } });
+    // const btnBuscar = screen.getByTestId('exec-search-btn');
+    // userEvent.click(btnBuscar);
+    // mock();
+    // expect(alert).toBeDefined();
+  });
+
+  it.skip('Verifica a tela Foods há botoes de categorias', async () => {
     renderWithRouter(
       <RecipesProvider>
         <Foods />
@@ -35,37 +58,22 @@ describe('Foods', () => {
     await screen.findAllByRole('button');
   });
 
-  it('Verifica a tela Foods aparece mensagem de receita não encontrada', async () => {
+  it.skip('Verifica a tela Foods aparece mensagem de receita não encontrada', async () => {
     renderWithRouter(
       <RecipesProvider>
         <Foods />
       </RecipesProvider>,
     );
+
     const searchBtn = screen.getByTestId('search-top-btn');
     userEvent.click(searchBtn);
     const name = screen.getByLabelText('Name');
     userEvent.click(name);
     const inputSearch = screen.getByTestId('search-input');
-    fireEvent.change(inputSearch, { target: { value: '/////' } });
+    userEvent.type(inputSearch, 'hello');
     const btnBuscar = screen.getByTestId('exec-search-btn');
     userEvent.click(btnBuscar);
-    mock();
-    expect(alert).toBeDefined();
-  });
-
-  it('Verifica se ao clicar na receita encaminha para details', async () => {
-    renderWithRouter(
-      <RecipesProvider>
-        <Foods />
-      </RecipesProvider>,
-    );
-    screen.findAllByTestId('0-card-name');
-    // const name = screen.getByLabelText('Name');
-    // const inputSearch = screen.getByTestId('search-input');
-    // fireEvent.change(inputSearch, { target: { value: '/////' } });
-    // const btnBuscar = screen.getByTestId('exec-search-btn');
-    // userEvent.click(btnBuscar);
-    // mock();
-    // expect(alert).toBeDefined();
+    global.alert = jest.fn()
+      .mockReturnValue(alert);
   });
 });
